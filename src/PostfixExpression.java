98cda414c6bdd18e8d939943d93f48f20260c80a
Soravaki <92048405+Soravaki@users.noreply.github.com>
2021-11-11 13:57:57 -0600

    School Work

--- src/PostfixExpression.java
+++ src/PostfixExpression.java
@@ -1,2 +1,34 @@
+import java.util.Stack;
 public class PostfixExpression {
+    Stack<String> s;
+    String expression;
+    String[] character;
+    public PostfixExpression(String expression) {
+        this.expression = expression;
+        character = expression.split("");
+    }
+
+    public void checkValues(){
+        s = new Stack<>();
+        for (String value : character){
+            if (DigitCheck(value)){
+                s.push(value);
+            }
+            else{
+                int num1 = Integer.parseInt(s.pop());
+                int num2 = Integer.parseInt(s.pop());
+                switch(value){
+                    // Change to string when work
+                    case "+"->{s.push(num1 + num2);}
+                    case "-"->{s.push(num1 - num2);}
+                }
+            }
+        }
+    }
+
+    public boolean DigitCheck(String character){
+        //System.out.println("'"+ character+"' is a number");
+        //System.out.println("'"+ character +"' is a letter");
+        return Character.isDigit(character.charAt(0));
+    }
 }
